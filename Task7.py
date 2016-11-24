305ca7d63697ef81b79baae05a5f7afa0256309c
Parn25 <mkv-rtf@mail.ru>
2016-11-24 10:50:21 +0300

    Убрал циклы. Заменил стандартными функциями.

--- Task7.py
+++ Task7.py
@@ -17,33 +17,25 @@ def test(start):
         start.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/table/tbody/tr[1]/td[2]/span/input').send_keys('admin')
         start.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/table/tbody/tr[2]/td[2]/span/input').send_keys('admin')
         start.find_element_by_xpath('//*[@id="box-login"]/form/div[2]/button').click()
-        """Находим все пункт меню"""
+        #Находим все пункт меню
         all_link = start.find_elements_by_css_selector('#box-apps-menu li')
-        """Подсчитываем их количество"""
-        l=0
-        for el in all_link:
-            l+=1
-        """Кликаем по родительскому пункту меню"""
-        for li_web in [ str(li_num) for li_num in range(1,l+1)]:
+        #Кликаем по родительскому пункту меню
+        for li_web in [ str(li_num) for li_num in range(1,len(all_link)+1)]:
             link_st='//*[@id="box-apps-menu"]/li['+li_web+']'
             start.find_element_by_xpath(link_st).click()
-            """Проверям заголовок у родительского пункта меню"""
+            #Проверям заголовок у родительского пункта меню
             try:
                 start.find_element_by_xpath('//*[@id="content"]/h1/span')
             except NoSuchElementException:
                 start.quit()
-            """Получаем список дочерних пунктов меню"""
+            #Получаем список дочерних пунктов меню"""
             dt_li=start.find_elements_by_xpath(link_st+'/ul/li')
-            """Если есть дочернии пункты меню кликаем по ним"""
+            #Если есть дочернии пункты меню кликаем по ним"""
             if dt_li != []:
-                """Подсчитываем количестов дочерних пунктов меню"""
-                d=0
-                for er in dt_li:
-                    d += 1
-                """Кликаем по дочерним пунктам меню"""
-                for dt_clik in [ str(li_num1) for li_num1 in range(1,d+1)]:
+                #Кликаем по дочерним пунктам меню"""
+                for dt_clik in [ str(li_num1) for li_num1 in range(1,len(dt_li)+1)]:
                     start.find_element_by_xpath(link_st+'/ul/li['+dt_clik+']').click()
-                    """Проверяем заголовок дочерних пунктов меню"""
+                    #Проверяем заголовок дочерних пунктов меню"""
                     try:
                         start.find_element_by_xpath('//*[@id="content"]/h1/span')
                     except NoSuchElementException:
--- Task8.py
+++ Task8.py
@@ -14,14 +14,8 @@ def start(request):
     return driver
 def test(start):
         start.get('http://localhost/litecart/en/')
-        most_product = start.find_elements_by_css_selector('#box-most-popular li.product') #Находми все товары в этой секции
-        most_sticker = start.find_elements_by_css_selector('#box-most-popular div.sticker') #Находми все стикеры в этой секции
-        assert len(most_product) == len(most_sticker) #Если число товаров и стикеров  не совпадает возбуждаем исключение
-        #Проверяем секцию Campaigns
-        cam_product = start.find_elements_by_css_selector('#box-campaigns li.product')
-        cam_sticker= start.find_elements_by_css_selector('#box-campaigns div.sticker')
-        assert len(cam_product) == len(cam_sticker)
-        #Проверяем секцию Latest Products
-        last_product = start.find_elements_by_css_selector('#box-latest-products li.product')
-        last_sticker = start.find_elements_by_css_selector('#box-latest-products div.sticker')
-        assert len(last_product) == len(last_sticker)
+        product = start.find_elements_by_css_selector('li.product')#Находми все товары
+        for i in product: #Итерируемся по все товарам и проверяем у каждого товара наличие по одному стикеру
+            time.sleep(5)
+            sticker=i.find_elements_by_css_selector('div.sticker')
+            assert len(sticker) == 1
