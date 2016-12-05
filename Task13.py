3e1745085e9ccab4fddb2b21abe79904c1a32c50
Parn25 <mkv-rtf@mail.ru>
2016-12-05 15:44:24 +0300

    Задание 13

--- Task13.py
+++ Task13.py
@@ -53,5 +53,5 @@ def test(start):
         if rd == '2': start.find_element_by_css_selector('#box-checkout-cart > ul > li:nth-child(2) > a').click()#После удаления одного товара товары не переключаются. Возможно дефект.
         WebDriverWait(start, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'li.item:nth-child(' + rd + ') button[value="Remove"]'))).click()#Дожидаемся когда появится нужный нам товар и удаляем его
         rd_1 = str(int(rd) + 1)
-        WebDriverWait(start, 10).until(EC.invisibility_of_element_located((By.XPATH, '//*[@id="order_confirmation-wrapper"]/table/tbody/tr[' + rd_1 + ']/td[3]')))#Дожидаемся пока обновится таблтца внизу
+        WebDriverWait(start, 10).until(EC.invisibility_of_element_located((By.XPATH, '//*[@id="order_confirmation-wrapper"]/table/tbody/tr[' + rd_1 + ']/td[3]')))#Дожидаемся пока обновится таблица внизу
 
