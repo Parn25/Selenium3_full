6d2384b853e64527b1ef043f108cd26cef5cfad4
Parn25 <mkv-rtf@mail.ru>
2016-12-06 21:04:13 +0300

    Задание 13

--- simple_test.py
+++ simple_test.py
@@ -2,52 +2,36 @@
 #Версия Python 2.7
 import pytest
 from selenium import webdriver
+import copy
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+
 import time
-wd = webdriver.Chrome()
-wd.get('http://localhost/litecart/en/')
-product_1 = wd.find_elements_by_css_selector('li.product')
-for pr in product_1:
-    if  pr.find_element_by_css_selector('a div.name').get_attribute('textContent') == "Yellow Duck":
-        pr.find_element_by_css_selector('a.link').click()
-        Select(wd.find_element_by_css_selector('div.information select')).select_by_visible_text('Small')
-        after_prop_1 = wd.find_element_by_css_selector('#cart > a.content > span.quantity').get_attribute('textContent')
-        wd.find_element_by_css_selector('td.quantity button').click()
-        WebDriverWait(wd, 10).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#cart > a.content > span.quantity"),str(int(after_prop_1)+1)))
-        break
 
-wd.back()
-product_2 = wd.find_elements_by_css_selector('li.product')
-for pr2 in product_2:
-    if  pr2.find_element_by_css_selector('a div.name').get_attribute('textContent') == "Blue Duck":
-        pr2.find_element_by_css_selector('a.link').click()
-        after_prop_12 = wd.find_element_by_css_selector('#cart > a.content > span.quantity').get_attribute('textContent')
-        wd.find_element_by_css_selector('td.quantity button').click()
-        WebDriverWait(wd, 10).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#cart > a.content > span.quantity"),str(int(after_prop_12)+1)))
-        break
-
-wd.back()
-product_3 = wd.find_elements_by_css_selector('li.product')
-for pr3 in product_3:
-    if  pr3.find_element_by_css_selector('a div.name').get_attribute('textContent') == "Green Duck":
-        pr3.find_element_by_css_selector('a.link').click()
-        after_prop_13 = wd.find_element_by_css_selector('#cart > a.content > span.quantity').get_attribute('textContent')
-        wd.find_element_by_css_selector('td.quantity button').click()
-        WebDriverWait(wd, 10).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#cart > a.content > span.quantity"),str(int(after_prop_13)+1)))
-        break
-
-
-wd.find_element_by_css_selector('#cart > a.link').click()
-all_product_order=wd.find_elements_by_css_selector('li.item')
-ft=[ str(li_num) for li_num in range(1,len(all_product_order)+1)]
-ft.reverse()
-for rd in ft:
-    if rd == '2': wd.find_element_by_css_selector('#box-checkout-cart > ul > li:nth-child(2) > a').click()
-    WebDriverWait(wd,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'li.item:nth-child('+rd+') button[value="Remove"]'))).click()
-    rd_1=str(int(rd)+1)
-    WebDriverWait(wd,10).until(EC.invisibility_of_element_located((By.XPATH,'//*[@id="order_confirmation-wrapper"]/table/tbody/tr['+rd_1+']/td[3]')))
+driver = webdriver.Chrome()
+driver.get('http://localhost/litecart/admin/login.php')
+driver.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/table/tbody/tr[1]/td[2]/span/input').send_keys('admin')
+driver.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/table/tbody/tr[2]/td[2]/span/input').send_keys('admin')
+driver.find_element_by_xpath('//*[@id="box-login"]/form/div[2]/button').click()
+driver.find_element_by_css_selector('#box-apps-menu li:nth-child(3)').click()
+driver.find_element_by_css_selector('table.dataTable tr.row td:nth-child(5) a').click()
+#tl=driver.find_element_by_css_selector('head > title').get_attribute('title')
+#print tl
+all_link = driver.find_elements_by_css_selector('#content > form a:not([id="address-format-hint"])')
+for lk in all_link:
+    mn_win = driver.current_window_handle
+    all_old_win = driver.window_handles
+    lk.click()
+    all_win = driver.window_handles
+    new_win_hn=list(set(all_win) - set(all_old_win))
+    WebDriverWait(driver, 10).until(EC.new_window_is_opened(new_win_hn))
+    print driver.current_window_handle
+    driver.switch_to.window(new_win_hn[0])
+    print driver.current_window_handle
+    driver.close()
+    driver.switch_to.window(mn_win)
+    print driver.current_window_handle
 
 
